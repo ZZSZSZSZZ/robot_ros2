c9a7c9f01861a94a4aa07281fadb5192f8938d81
ZZSZSZSZZ <13023184453@163.com>
2025-11-10 21:02:14 +0800

    更新了硬件接口

--- robot_hardware_interface/src/robot_hardware_interface.cpp
+++ robot_hardware_interface/src/robot_hardware_interface.cpp
@@ -14,7 +14,7 @@ hardware_interface::CallbackReturn RobotHardwareInterface::on_init(
     return CallbackReturn::ERROR;
   }
 
-  // tcp.send("init", 192.168xxx)
+  robot_ = std::make_unique<robot::Robot>(8080);
 
   const size_t total_joints = info_.joints.size();
   pos_commands_.resize(total_joints, 0.0);
@@ -22,12 +22,11 @@ hardware_interface::CallbackReturn RobotHardwareInterface::on_init(
   pos_states_.resize(total_joints, 0.0);
   vel_states_.resize(total_joints, 0.0);
 
-  // if (tcp.read(192.168xxx) == "ok")
-  // {
-  //   return CallbackReturn::SUCCESS;
-  // }
+  if (robot_->init_robot()){
+    return CallbackReturn::SUCCESS;
+  }
 
-  return CallbackReturn::SUCCESS;
+  return CallbackReturn::ERROR;
 }
 
 hardware_interface::CallbackReturn RobotHardwareInterface::on_configure(
@@ -76,50 +75,62 @@ hardware_interface::CallbackReturn RobotHardwareInterface::on_activate(
   vel_commands_ = vel_states_;
   joint_vel_states_ = vel_states_;
 
+  // if (robot_->enable_robot()){
+  //   return CallbackReturn::SUCCESS;
+  // }
+  // return CallbackReturn::ERROR
+
   return CallbackReturn::SUCCESS;
 }
 
 hardware_interface::CallbackReturn RobotHardwareInterface::on_deactivate(
   const rclcpp_lifecycle::State & /*previous_state*/)
 {
+  // if (robot_->disable_robot()){
+  //   return CallbackReturn::SUCCESS;
+  // }
+  // return CallbackReturn::ERROR
+
   return CallbackReturn::SUCCESS;
 }
 
 hardware_interface::return_type RobotHardwareInterface::read(
   const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/)
 {
-  pos_states_ = joint_pos_states_;
+  // pos_states_ = joint_pos_states_;
   vel_states_ = joint_vel_states_;
 
-  // vel_states_ = tcp.read(192.xxxx)
-
-  printf("read");
-  for (auto i = 0ul; i < pos_states_.size(); i++){
+  if (robot_->read(&pos_states_)){
+    printf("read");
+    for (auto i = 0ul; i < pos_states_.size(); i++){
     printf("%f\n", pos_states_[i]);
-    printf("%f\n", vel_states_[i]);
+    // printf("%f\n", vel_states_[i]);
+    }
+    
+    return hardware_interface::return_type::OK;
   }
 
-  return hardware_interface::return_type::OK;
+  return hardware_interface::return_type::ERROR;
 }
 
 hardware_interface::return_type RobotHardwareInterface::write(
   const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/)
 {
-  joint_pos_states_ = pos_commands_;
+  // joint_pos_states_ = pos_commands_;
   joint_vel_states_ = vel_commands_;
 
-  // tcp.send(pos_commands_, 192.168xxxx)
-  // json
-  // cmd : init, read, write
-  // value : {0.0, 0.0 ... }
+  // printf("write");
+  // for (auto i = 0ul; i < joint_pos_states_.size(); i++){
+  //   printf("%f\n", pos_commands_[i]);
+  //   printf("%f\n", vel_commands_[i]);
+  // }
 
-  printf("write");
-  for (auto i = 0ul; i < joint_pos_states_.size(); i++){
-    printf("%f\n", pos_commands_[i]);
-    printf("%f\n", vel_commands_[i]);
+  if (robot_->write(pos_commands_)){
+    
+    return hardware_interface::return_type::OK;
   }
 
-  return hardware_interface::return_type::OK;
+  return hardware_interface::return_type::ERROR;
 }
 
 }  // namespace robot_hardware_interface
